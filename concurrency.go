99e990d38997f369b8147f6eb51410e7752e5b38
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-03-16 18:37:24 -0600

    channels

---
+++ concurrency.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println(text)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	fmt.Println("hello")
+	wg.Add(1)
+
+	go say("world", &wg)
+
+	wg.Wait()
+
+	go func(text string) {
+		fmt.Println(text)
+	}("adios")
+
+	time.Sleep(time.Second * 3)
+}
